e777b52d791458ca90c3f7249528b75351dbad57
Wiktor Małyska <wiktormalyska03@gmail.com>
2024-10-17 10:38:26 +0200

    init

---
+++ src/main/java/ovh/wiktormalyska/pharmacysystembackend/PharmacySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package ovh.wiktormalyska.pharmacysystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacySystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacySystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ovh/wiktormalyska/pharmacysystembackend/PharmacySystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.wiktormalyska.pharmacysystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacySystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
