8ce054d8b0cbde54e27e5c0018f7c66003e38223
wiktormalyska <wiktormalyska03@gmail.com>
2024-12-09 15:57:48 +0100

    Add DataLoader

---
+++ src/main/java/ovh/wiktormalyska/pharmacysystembackend/dataloader/DataLoader.java
@@ -0,0 +1,30 @@
+package ovh.wiktormalyska.pharmacysystembackend.dataloader;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import ovh.wiktormalyska.pharmacysystembackend.administrator.Administrator;
+import ovh.wiktormalyska.pharmacysystembackend.administrator.AdministratorRepository;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    private AdministratorRepository administratorRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public DataLoader(AdministratorRepository administratorRepository) {
+        this.administratorRepository = administratorRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Administrator administrator = Administrator.builder().username("admin").password(passwordEncoder.encode("admin")).build();
+        System.out.println(administrator);
+        administratorRepository.findByUsername("admin").ifPresentOrElse(
+                admin -> System.out.println("Admin already exists"),
+                () -> administratorRepository.save(administrator)
+        );
+    }
+}
